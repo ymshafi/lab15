3ecc1808134c09231bec23ebb82e5965a7631f71
ymshafi <yusuf.m.shafi@gmail.com>
2014-11-07 15:40:40 -0600

    Done

--- 100pt.py
+++ 100pt.py
@@ -32,10 +32,32 @@ class MyApp:
 		
 		self.up = Button(self.myContainer1)
 		self.up.configure(text="Up", background= "green")
-		self.up.grid(row=0,column=0)
+		self.up.grid(row=0,column=1)
 					
 		# "Bind" an action to the first button												
 		self.up.bind("<Button-1>", self.moveUp)
+                
+                self.up = Button(self.myContainer1)
+		self.up.configure(text="Down", background= "green")
+		self.up.grid(row=1,column=1)
+					
+		# "Bind" an action to the first button												
+		self.up.bind("<Button-1>", self.moveDown)
+                
+                self.up = Button(self.myContainer1)
+		self.up.configure(text="Left", background= "green")
+		self.up.grid(row=1,column=0)
+					
+		# "Bind" an action to the first button												
+		self.up.bind("<Button-1>", self.moveLeft)
+		
+		  
+                self.up = Button(self.myContainer1)
+		self.up.configure(text="Right", background= "green")
+		self.up.grid(row=1,column=3)
+					
+		# "Bind" an action to the first button												
+		self.up.bind("<Button-1>", self.moveRight)
                 
 		  
 		# This creates the drawpad - no need to change this 
@@ -48,13 +70,57 @@ class MyApp:
 		global drawpad
                 x1,y1,x2,y2 = drawpad.coords(player)
 		# Get the coords of our target
-                drawpad.move(player,0,-10)
-    
+                if y1 > 10:
+                    drawpad.move(player,0,-10)
+                if y1 < 10:
+                    return
+                    
+        def moveDown(self, event):   
+		global player
+		global drawpad
+                x1,y1,x2,y2 = drawpad.coords(player)
+		# Get the coords of our target
+                if y2 < 320:
+                    drawpad.move(player,0,10)
+                if y2 > 319:
+                    return 
+        def moveLeft(self, event):   
+		global player
+		global drawpad
+                x1,y1,x2,y2 = drawpad.coords(player)
+		# Get the coords of our target
+                if x1 > 1:
+                    drawpad.move(player,-10,0)
+                if x1 < 1:
+                    return
+        def moveRight(self, event):   
+		global player
+		global drawpad
+                x1,y1,x2,y2 = drawpad.coords(player)
+		# Get the coords of our target
+                if x2 < 480:
+                    drawpad.move(player,10,0)
+                if x2 > 480:
+                    return  
          
         # Animate function that will bounce target left and right, and trigger the collision detection  
 	def animate(self):
-	    global target
 	    global direction
+            global drawpad
+            global player
+            global target
+            x1, y1, x2, y2 = drawpad.coords(target)
+            if x1 < 1:
+                direction = 1
+            if x2 > drawpad.winfo_width():
+                direction = -1
+            drawpad.move(target,direction,0)
+            didwehit = self.collisionDetect()
+            if didwehit == True:
+                return
+            else:
+                drawpad.after(1, self.collisionDetect)
+                drawpad.after(1, self.animate)
 	    tx1,ty1,tx2,ty2 = drawpad.coords(target)
 	    
 	    # Insert the code here to make the target move, bouncing on the edges    
@@ -63,7 +129,7 @@ class MyApp:
             
             
             #  This will trigger our collision detect function
-            didWeHit = self.collisionDetect()
+            didwehit = self.collisionDetect()
             # Use the value of didWeHit to create an if statement
             # that determines whether to run drawpad.after(1,self.animate) or not
             
@@ -73,7 +139,12 @@ class MyApp:
 		global drawpad
                 global player
                 # Get the co-ordinates of our player AND our target
-                # using x1,y1,x2,y2 = drawpad.coords(object)
+                x1,y1,x2,y2 = drawpad.coords(player)
+                tx1,ty1,tx2,ty2 = drawpad.coords(target)
+                if (x1 > tx1 and x2 < tx2) and (y1 > ty1 and y2 < ty2):
+                    return True
+                else:
+                    return False 
 
                 # Do your if statement - remember to return True if successful!                
 		
